4633e52483381d63a0e108bf7a9dfc32dc37f0d5
Oleg Knyazev <knyazev751@gmail.com>
2017-07-06 09:25:06 +0300

    Simplify camera placement calculation

--- Assets/GraphicToRT.cs
+++ Assets/GraphicToRT.cs
@@ -17,14 +17,6 @@ namespace UIToRenderTarget {
         Canvas _localCanvas;
         GraphicRaycaster _localRaycaster;
 
-        // in world space
-        Vector2 _graphicSize;
-        Vector3 _graphicCenter;
-        Vector3 _graphicNormal;
-        Quaternion _graphicRotation;
-
-        static readonly Vector3[] s_worldCorners = new Vector3[4];
-
         public void OnEnable() {
             _graphic = GetComponent<Graphic>();
             _rectTransform = GetComponent<RectTransform>();
@@ -40,7 +32,6 @@ namespace UIToRenderTarget {
         public void Update() {
             UpdateLocalCanvas();
             Utils.WithoutScaleAndRotation(_graphic.transform, () => {
-                UpdateGraphicMetrics();
                 UpdateRTSize();
                 UpdateRT();
             });
@@ -50,6 +41,12 @@ namespace UIToRenderTarget {
         public void OnGUI() {
             if (_rt) GUI.DrawTexture(new Rect(0, 0, _rt.width, _rt.height), _rt);
         }
+
+        Rect graphicRect { get { return _rectTransform.rect; } }
+        Quaternion graphicRotation { get { return _rectTransform.rotation; } }
+        Vector3 graphicNormal { get { return _rectTransform.forward; } }
+        Vector3 graphicCenter { get { return _rectTransform.TransformPoint(graphicRect.center); } }
+        Vector2 graphicSize { get { return graphicRect.size; } }
 
         Camera CreateCamera() {
             var camera =
@@ -76,20 +73,9 @@ namespace UIToRenderTarget {
             }
         }
 
-        void UpdateGraphicMetrics() {
-            var wc = s_worldCorners;
-            _rectTransform.GetWorldCorners(wc);
-            var xAxis = wc[3] - wc[0];
-            var yAxis = wc[1] - wc[0];
-            _graphicSize = new Vector2(xAxis.magnitude, yAxis.magnitude);
-            _graphicCenter = (wc[0] + wc[1] + wc[2] + wc[3]) / 4;
-            _graphicNormal = Vector3.Cross(xAxis, yAxis).normalized;
-            _graphicRotation = Quaternion.FromToRotation(Vector3.forward, _graphicNormal);
-        }
-
         void UpdateRTSize() {
-            var desiredWidth = (int)_graphicSize.x;
-            var desiredHeight = (int)_graphicSize.y;
+            var desiredWidth = (int)graphicSize.x;
+            var desiredHeight = (int)graphicSize.y;
             if (!_rt || _rt.width != desiredWidth || _rt.height != desiredHeight) {
                 if (_rt)
                     DestroyImmediate(_rt);
@@ -103,10 +89,10 @@ namespace UIToRenderTarget {
             if (_rt && _localCanvas.rootCanvas) {
                 Utils.WithRenderMode(_localCanvas.rootCanvas, RenderMode.WorldSpace, () => 
                 Utils.WithObjectLayer(_graphic.gameObject, layer, () => {
-                    _camera.orthographicSize = _graphicSize.y / 2;
-                    _camera.aspect = _graphicSize.x / _graphicSize.y;
-                    _camera.transform.position = _graphicCenter - _graphicNormal * _camera.nearClipPlane * 2f;
-                    _camera.transform.rotation = _graphicRotation;
+                    _camera.orthographicSize = graphicSize.y / 2;
+                    _camera.aspect = graphicSize.x / graphicSize.y;
+                    _camera.transform.position = graphicCenter - graphicNormal * _camera.nearClipPlane * 2f;
+                    _camera.transform.rotation = graphicRotation;
                     _camera.targetTexture = _rt;
                     _camera.cullingMask = 1 << layer;
                     _camera.Render();
